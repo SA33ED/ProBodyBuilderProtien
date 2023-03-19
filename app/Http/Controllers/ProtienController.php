494081b091421c4b137918b3d919bed70c7395fe
SA33ED <mohammedsaeed892002@gmail.com>
2023-03-19 21:02:23 +0200

    create protien model

---
+++ app/Http/Controllers/ProtienController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Protien;
+use App\Http\Requests\StoreProtienRequest;
+use App\Http\Requests\UpdateProtienRequest;
+
+class ProtienController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProtienRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Protien $protien)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Protien $protien)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProtienRequest $request, Protien $protien)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Protien $protien)
+    {
+        //
+    }
+}
